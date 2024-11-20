9403f7564f6bf8818ac55a056e83db7c376da2c1
yogadwipayana <yogadwipayana2006@gmail.com>
2024-11-20 16:22:15 +0800

    menambahkan nav dan banner

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("hello")
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
